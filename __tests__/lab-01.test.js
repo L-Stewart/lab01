996f9745e7ac9654546e066a5bac642bde894f76
LorinStewart <stewartalorin@gmail.com>
2019-02-09 10:44:41 -0800

    added in the arithmetic module and the ad and subtract functions

--- __tests__/lab-01.test.js
+++ __tests__/lab-01.test.js
@@ -3,42 +3,60 @@
 //Lorin - Test module files.
 // const arithmetic = require('../lib/arithmetic.js');
 const greet = require('../lib/greet.js');
+const math = require('../lib/math.js');
 
 //Lorin - load npm packages.
 const faker = require('faker');
 
 describe('#demo.test.js', () => {
-  // Vinicio - here, we'll write all our tests
-  test('These are the basics of TDD', () =>{
-    // ARRANGE
-    const aNumber = 5;
-    const aNullValue = null;
-    const fifty = 50;
+  //  Lorin- demo code from Vinicio
+    // // Vinicio - here, we'll write all our tests
+    // test('These are the basics of TDD', () =>{
+    //   // ARRANGE
+    //   const aNumber = 5;
+    //   const aNullValue = null;
+    //   const fifty = 50;
+    //
+    //   // ACT
+    //   // Vinicio - no act, because we are just showing basics
+    //
+    //   // ASSERT
+    //   expect(aNumber).toEqual(5);
+    //   expect(fifty).toBeLessThan(100);
+    //   expect(fifty).toBeGreaterThan(10);
+    //   expect(aNullValue).toBeNull();
+    //   const firstArgument = faker.random.number();
+    //   const secondArgument = faker.random.number();
+    //   // expect(myModule.myFunction(firstArgument, secondArgument).toEqual(firstArgument + secondArgument));
+    // });
+    //
+    // test('Showcasing how to add another test ', () => {
+    //   //ARRANGE
+    //   const aTruthyValue = 'hi';
+    //   const aFalsyValue = 0;
+    //
+    //   //ACT :(
+    //
+    //   //ASSERT
+    //   expect(aTruthyValue).toBeTruthy();
+    //   expect(aFalsyValue).toBeFalsy();
+    //   expect(aTruthyValue).not.toBeFalsy();
+    // });
 
-    // ACT
-    // Vinicio - no act, because we are just showing basics
-
-    // ASSERT
-    expect(aNumber).toEqual(5);
-    expect(fifty).toBeLessThan(100);
-    expect(fifty).toBeGreaterThan(10);
-    expect(aNullValue).toBeNull();
-    const firstArgument = faker.random.number();
-    const secondArgument = faker.random.number();
-    // expect(myModule.myFunction(firstArgument, secondArgument).toEqual(firstArgument + secondArgument));
-  });
-
-  test('Showcasing how to add another test ', () => {
-    //ARRANGE
-    const aTruthyValue = 'hi';
-    const aFalsyValue = 0;
-
-    //ACT :(
+  test('math.add/subtract should complete if the input is aa number', () => {
+    //ARRANGE :(
+    //ACT
+    const add = math.add(1, 2);
+    const addNull = math.add(1, 'd');
+    const subtract = math.subtract(1, 2);
+    const subtractNull = math.subtract(3, 'w');
 
     //ASSERT
-    expect(aTruthyValue).toBeTruthy();
-    expect(aFalsyValue).toBeFalsy();
-    expect(aTruthyValue).not.toBeFalsy();
+    expect(add).toEqual(3);
+    expect(addNull).toEqual(null);
+    expect(subtract).toEqual(-1);
+    expect(subtractNull).toEqual(null);
+
   });
 
   test('greet.sayHi should say hello if the input type is a string, otherwise it will return null', () => {
